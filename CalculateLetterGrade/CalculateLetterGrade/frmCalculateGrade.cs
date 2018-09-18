7cf41a342520aafb245089f2cacda9a8172bb837
Jin Tang <42655902+xiaolan706@users.noreply.github.com>
2018-09-17 20:08:28 -0600

    Add files via upload

--- CalculateLetterGrade/CalculateLetterGrade/frmCalculateGrade.cs
+++ CalculateLetterGrade/CalculateLetterGrade/frmCalculateGrade.cs
@@ -17,39 +17,50 @@ namespace CalculateLetterGrade
             InitializeComponent();
         }
 
+        // function for Calculate button Click
         private void btnCalculate_Click(object sender, EventArgs e)
         {
+            // get input from the user, and convert string txtNumberGrade to decimal
             decimal numberGrade = Convert.ToDecimal(txtNumberGrade.Text);
 
+            // initialize string letterGrade to ""
             string letterGrade = "";
-            if (numberGrade >= 88)
+            // if decimal numberGrade is between 88 to 100
+            if (numberGrade >= 88 && numberGrade <= 100) 
             {
-                letterGrade = "A";
+                letterGrade = "A"; // string letterGrade is "A"
             }
+            // if decimal numberGrade is between 80 to 87
             else if (numberGrade >= 80 && numberGrade <= 87)
             {
-                letterGrade = "B";
+                letterGrade = "B"; // string letterGrade is "B"
             }
+            // if decimal numberGrade is between 68 to 79
             else if (numberGrade >= 68 && numberGrade <= 79)
             {
-                letterGrade = "C";
+                letterGrade = "C"; // string letterGrade is "C"
             }
+            // if decimal numberGrade is between 60 to 67
             else if (numberGrade >= 60 && numberGrade <= 67)
             {
-                letterGrade = "D";
+                letterGrade = "D"; // string letterGrade is "D"
             }
-            else
+            // if decimal numberGrade is not in range [60, 100]
+            else 
             {
-                letterGrade = "F";
+                letterGrade = "F"; // string letterGrade is "F"
             }
 
+            // change the value of txtLetterGrade to letterGrade  
             txtLetterGrade.Text = letterGrade;
-            txtNumberGrade.Focus();
+            // move the focus back to txtNumberGrade
+            txtNumberGrade.Focus(); 
         }
 
+        // function for Exit button Click
         private void btnExit_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close(); // close the form
         }
     }
 }
